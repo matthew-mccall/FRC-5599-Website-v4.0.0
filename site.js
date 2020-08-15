1f8af2197c43005826f3a91b87d4046c1b469695
Matthew McCall <matthew@mccall.app>
2020-08-15 18:50:42 -0400

    Swited to loading data from team.json

--- site.js
+++ site.js
@@ -1,4 +1,20 @@
+const fs = require('fs')
+
+var rawData;
+var data;
+
 exports.index = function (req, res) {
-    //TODO: Query database for information and pass that into main page.
-    res.render('index.html')
+
+    try {
+        rawData = fs.readFileSync('data/team.json', 'utf8')
+        data = JSON.parse(rawData)
+    } catch (err) {
+        console.error(err)
+    }
+
+    res.render('index.html', {
+        desc: data.desc,
+        history: data.history
+    });
+
 }
