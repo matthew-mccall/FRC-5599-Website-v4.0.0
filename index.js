c0b7b6955a503f6e21e7a58ea4e73578bcc01455
Matthew McCall <matthew@mccall.app>
2020-08-15 16:24:56 -0400

    Refactored main and competition response into file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const site = require('./site')
+const competitions = require('./competitions')
+
 const app = module.exports = express();
 const port = process.env.PORT | 3000
 
@@ -6,11 +9,10 @@ app.use(express.static('www'))
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
 
-app.get('/', (req, res) => {
-    //TODO: Query database for information and pass that into main page.
-    res.render('index.html')
-})
+app.get('/', site.index)
+app.get('/competitions', competitions.index)
 
+// Maybe extract these into separate files too?
 app.use(function (req, res, next) {
     res.status(404)
     res.render('404.html')
---
+++ site.js
@@ -0,0 +1,4 @@
+exports.index = function (req, res) {
+    //TODO: Query database for information and pass that into main page.
+    res.render('index.html')
+}
