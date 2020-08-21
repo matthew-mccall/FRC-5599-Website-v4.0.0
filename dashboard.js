b91e63143a326e60c266842b559fab10c2db556e
Matthew McCall <matthew@mccall.app>
2020-08-21 14:01:03 -0400

    Added support for making announcements

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
 const fs = require('fs')
 var dashboard = express.Router();
 
@@ -11,9 +12,22 @@ var teamData;
 var announcementData;
 var members;
 
+dashboard.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+)
+
+dashboard.use(bodyParser.json())
+
 dashboard.get('/', function (req, res) {
 
     if (req.session.user) {
+
+        if (!req.session.user.isBoard && !req.session.user.isMentor) {
+            res.redirect('profile')
+            return;
+        }
 
         try {
             rawTeamData = fs.readFileSync('data/team.json', 'utf8')
@@ -31,19 +45,54 @@ dashboard.get('/', function (req, res) {
                 if (err) throw err
                 members = result;
                 db.close()
+                res.render('dashboard', {
+                    user: req.session.user,
+                    team: teamData,
+                    announcement: announcementData,
+                    members: members
+                })
             })
-        })
-
-        res.render('dashboard', {
-            user: req.session.user,
-            team: teamData,
-            announcement: announcementData,
-            members: members
         })
     } else {
         res.redirect('/signin')
     }
 
+})
+
+dashboard.post('/', function (req, res) {
+
+    if (req.session.user == undefined) {
+        res.redirect('/signin')
+        return;
+    }
+
+    if (req.body.announcementMessage) {
+        announcementData.message = req.body.announcementMessage
+        try {
+            fs.writeFileSync('data/announcement.json', JSON.stringify(announcementData))
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    if (req.body.announcementDisplay) {
+        announcementData.display = req.body.announcementDisplay
+        try {
+            fs.writeFileSync('data/announcement.json', JSON.stringify(announcementData))
+        } catch (err) {
+            console.error(err)
+        }
+    } else {
+        announcementData.display = false;
+        fs.writeFileSync('data/announcement.json', JSON.stringify(announcementData))
+    }
+
+    res.render('dashboard', {
+        user: req.session.user,
+        team: teamData,
+        announcement: announcementData,
+        members: members
+    })
 })
 
 module.exports = dashboard;
